62e2902c1f4809152974d37c28f3f2ca42f5d10f
Carlos Godoy <carlosgodoy@pursuit.org>
2022-08-24 23:16:31 -0400

    adding sections

---
+++ app.js
@@ -0,0 +1,22 @@
+// Reading an Element from the Document
+
+
+
+// Removing an Element
+
+
+
+// Creating an Element
+// Giving the Element Content
+// Appending the Element to another Element
+
+
+// Adding and Event Listener
+// Changing the styling of an Element
+
+
+
+
+// Adding an event listener to a Form
+// Preventing default behavior using event.preventDefault()
+// Getting the 'value' from an input
